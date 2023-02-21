ab1f0cc7e5f6c5e9a4985d9a56b472c4840a7dbc
Beanie95 <phamhien7295@gmail.com>
2023-02-21 12:57:38 +0700

    Update Bai 12

---
+++ src/javaTester/Topic_01_System.java
@@ -0,0 +1,15 @@
+package javaTester;
+
+public class Topic_01_System {
+
+	public static void main(String[] args) {
+		String proPath = System.getProperty("user.dir"); //duong dan cua project
+		System.out.println(proPath);
+		
+		String osName = System.getProperty("os.name");	// ten he dieu hanh
+		System.out.println(osName);
+	
+
+	}
+
+}
